846be045a0415a80f4206f1b6b3c23af803dfb62
damuM <damulearnings@gmail.com>
2026-03-31 13:25:28 +0530

    added imports for AI Deployment 3

--- Infra/AI_Services_Cognitive_Services_deployment/imports.tf
+++ Infra/AI_Services_Cognitive_Services_deployment/imports.tf
@@ -1,14 +1,4 @@
 
-import {
-  id="/subscriptions/bf6c9b18-a592-4790-930a-9bff827fe111/resourceGroups/RG-QA-AI-Service-Resources"
-  to=azurerm_resource_group.ai-D
-  
-}
-
-import {
-  id="/subscriptions/bf6c9b18-a592-4790-930a-9bff827fe111/resourceGroups/RG-QA-AI-Service-Resources/providers/Microsoft.CognitiveServices/accounts/oai-qa-service-account-d"
-  to=azurerm_cognitive_account.ai-service-OpenAI-D 
-}
 
 
 
